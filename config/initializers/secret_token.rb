d2930b731b50b4053329bfbd1ba70902afa0ceb4
Andrew Bakalyar <andrew.bakalyar@gmail.com>
2013-05-07 11:17:40 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Richlang::Application.config.secret_token = 'ca48a16e6efeee1dee22b7d1b4f4c7a9ea6d20effd9a23e00703a16868cc2f1c4bf311210ddda780328660f6093f93b102866496fde44c5d7a0437c744ad725b'
