782f5a23ea9beaef4f4a396cb50a262d00c8b165
Tima Maslyuchenko <insside@gmail.com>
2013-06-20 11:24:04 +0300

    fixed specs

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -3,30 +3,37 @@ require 'spec_helper'
 describe "Words" do
   include Warden::Test::Helpers
   Warden.test_mode!
-
-  user = FactoryGirl.create(:user)
-  word = FactoryGirl.create(:word, user: user, word: 'hello_uk', translation: 'hello_ua')
+  
+  let(:word) { FactoryGirl.create(:word, word: 'hello_uk', translation: 'hello_ua') }
+  let(:user) { FactoryGirl.create(:user, words: [word]) }
 
   before(:each) do
     login_as(user, :scope => :user)
-    visit words_path
+  end
+  
+  describe 'words listing' do
+    it "shows page" do
+      visit words_path
+      
+      expect(page).to have_field('search')
+
+      expect(page).to have_link('add_link')
+      expect(page).to have_content(user.words.count)
+      expect(page).to have_link(word.word)
+      expect(page).to have_link(word.translation)
+      expect(page).to have_link('delete_link')
+    end
   end
 
-  it "list page" do
-    expect(page).to have_field('search')
 
-    expect(page).to have_link('add_link')
-    expect(page).to have_content(user.words.count)
-    expect(page).to have_link(word.word)
-    expect(page).to have_link(word.translation)
-    expect(page).to have_link('delete_link')
-  end
-
-  it "adding word", :js => true do
-    click_link('add_link')
-    #print page.html
-    #expect(page).to have_сss('#input_word')
-    expect(page).to have_field('input_word')
+  describe 'adding new words' do
+    it "adding word", js: true do
+      visit words_path
+      
+      click_link('add_link')
+      
+      expect(page).to have_field('input_word')
+    end
   end
 
   Warden.test_reset!
