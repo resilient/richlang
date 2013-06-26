0b7b8169586c1dfbdea9a128bc56360c51da3dfd
Andriy <resilient@bigmir.net>
2013-06-26 13:36:34 +0300

    model spec for search

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -10,4 +10,12 @@ describe Word do
   it { should validate_presence_of(:word).with_message(I18n.t('.enter_word')) }
   it { should validate_presence_of(:translation).with_message(I18n.t('.enter_translation')) }
 
+  it "returns matched results of search" do
+    hello = FactoryGirl.create(:word, word: 'hello_uk', translation: 'hello_ua')
+    hi = FactoryGirl.create(:word, word: 'hi_uk', translation: 'hi_ua')
+    hey = FactoryGirl.create(:word, word: 'hey_uk', translation: 'hey_ua')
+
+    Word.search("he").should == [hello, hey]
+  end
+
 end
